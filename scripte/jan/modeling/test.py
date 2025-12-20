32a3694e59d0ef9e5d0d76f18014a45099aae6cb
Jan-Kirschke <mail@jankirschke.de>
2025-12-20 13:49:01 +0100

    Habe staedte_statistik.json hinzugefügt, diese Datei beinhaltet die Eckdaten der scraping Dateien, wie "von", "bis" und Anzahl Datensätze pro Datei.

--- scripte/jan/modeling/test.py
+++ scripte/jan/modeling/test.py
@@ -5,12 +5,30 @@ file_path = os.path.dirname(__file__)
 jan_path = os.path.join(file_path, "..")
 scripte_path = os.path.join(jan_path, "..")
 main_path = os.path.join(scripte_path, "..")
-daten_sets_path = os.path.join(main_path, "Daten_sets/EUDA_Wastewater_analysis_and_drugs/ww2025_deutschland.csv")
+daten_sets_path = os.path.join(main_path, "Daten_sets/blaulicht_scraping")
 
-print("")
-df = pd.read_csv(daten_sets_path)
+staedte_liste = []
+for filename in os.listdir(daten_sets_path):
+    print(filename)
+    if filename.startswith("."):
+        continue
+    df = pd.read_csv(os.path.join(daten_sets_path, filename), encoding="utf_8")
+
+    df["datum"] = pd.to_datetime(df["datum"], format="%d.%m.%Y – %H:%M", errors='coerce')
+    stadt_info = {
+            "filename": filename,
+            "stadt": df["stadt"].iloc[0] if not df.empty else "Unbekannt",
+            "von": df["datum"].min(),
+            "bis": df["datum"].max(),
+            "anzahl_posts": len(df)
+        }
+        
+        # 3. Dictionary der Liste hinzufügen
+    staedte_liste.append(stadt_info)
 
 
-max_drugs_index = df.idxmax()
-max_drugs = df.loc[max_drugs_index]
-print(max_drugs)
+df_final = pd.DataFrame(staedte_liste)
+df_final.to_json(os.path.join(daten_sets_path,"staedte_statistik.json"), orient="records",force_ascii=False, indent=4, date_format="iso")
+print(staedte_liste)
+
+
