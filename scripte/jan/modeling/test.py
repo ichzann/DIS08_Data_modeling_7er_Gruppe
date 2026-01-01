47f08f94b6d065286413ff51d0653b23dd8f9861
Jan-Kirschke <mail@jankirschke.de>
2026-01-01 12:39:07 +0100

    grouped the data by drug. Added csv files of the drugs

--- scripte/jan/modeling/test.py
+++ scripte/jan/modeling/test.py
@@ -5,23 +5,30 @@ import pandas as pd
 script_dir = os.path.dirname(os.path.abspath(__file__))
 data_sets_folder_path = os.path.join(script_dir, "../../../Daten_sets")
 euda_sets_path = os.path.join(data_sets_folder_path, "EUDA_Wastewater_analysis_and_drugs")
+filtered_by_drug_data_sets = os.path.join(euda_sets_path, "filtered_by_drug")
 
 
 file_name = "ww2025-all-data.csv" 
 df = pd.read_csv(os.path.join(euda_sets_path, "ww2025-all-data.csv"))
 
 
-df_c = df[df["Metabolite"] == "cocaine"]
-df_c = df_c[["Year", "City", "Daily mean"]]
+def nach_droge_segmentieren(df, drogenname: str):
+    df_droge = df[df["Metabolite"] == drogenname]
+
+    df_c = df_droge[["Year", "City", "Daily mean"]]
 
 
-df_transformed = df_c.pivot_table(index='Year', columns='City', values='Daily mean', aggfunc='mean')
+    df_transformed = df_c.pivot_table(index='Year', columns='City', values='Daily mean', aggfunc='mean')
 
-df_transformed = df_transformed.reset_index()
+    df_transformed = df_transformed.reset_index()
 
-df_transformed.columns.name = None
+    df_transformed.columns.name = None
 
-print(df_transformed.head())
-print(df_transformed.head(3))
+    df_transformed.to_csv(os.path.join(filtered_by_drug_data_sets, f"ww2025_{drogenname}_pivoted.csv"), index=False)
+    print("Gespeichert:", f"ww2025_{drogenname}_pivoted.csv")
 
-df_transformed.to_csv(os.path.join(euda_sets_path, "ww2025_cocaine_pivoted.csv"), index=False)
+drugs = df["Metabolite"].unique().tolist()
+
+
+for droge in drugs:
+    nach_droge_segmentieren(df, droge)
